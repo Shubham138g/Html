6353171c4b108bab151ec51bef192a665222d34f
shubham <tonygupta275@gmail.com>
2023-08-11 20:48:12 +0530

    modify data and time

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 
 
 const name = "Shubham";
-const date = new Date().getDate();
-const month = new Date().getMonth();
-const year = new Date().getFullYear();
-const time = new Date().toTimeString();
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
 function App() {
   return (
     <>
       <center><h1 className="h1">Hello {name} </h1></center>
-      <p className="p">{`Today's Date:${date}/${month}/${year}`}</p>
+      <p className="p">{`Today's Date:${date}`}</p>
       <p className="p">{`Today's Time: ${time}`}</p>
     </>
   );
