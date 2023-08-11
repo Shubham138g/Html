8fa2ec1b029f8cb2c6ba1885a7a876f6f8df01e4
shubham <tonygupta275@gmail.com>
2023-08-11 21:04:16 +0530

    greeting logic

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -4,12 +4,31 @@ import './App.css';
 const name = "Shubham";
 const date = new Date().toLocaleDateString();
 const time = new Date().toLocaleTimeString();
+
+
+let currentDate = new Date();
+currentDate = currentDate.getHours();
+let greet = '';
+
+
+if (currentDate > 1 && currentDate < 12) {
+  greet = 'GOod Morning';
+}
+else if (currentDate > 12 && currentDate < 19) {
+  greet = 'Good Afternoon';
+}
+else if (currentDate > 19 && currentDate < 24) {
+  greet = 'Good night';
+}
+
+
 function App() {
   return (
     <>
       <center><h1 className="h1">Hello {name} </h1></center>
       <p className="p">{`Today's Date:${date}`}</p>
       <p className="p">{`Today's Time: ${time}`}</p>
+      <h1> Hello sir,{greet}</h1>
     </>
   );
 }
