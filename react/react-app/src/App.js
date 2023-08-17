81d6f6ec51130fed5f700047c0973964a825e5c0
shubham <tonygupta275@gmail.com>
2023-08-17 13:22:41 -0300

    increment number using usestate

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,24 +1,34 @@
 import './App.css';
 // import Greet from './Component/Greet';
 // import Props from './Component/Props';
-import CardExample from './Component/CardExample';
-import Sdata from './Sdata';
+// import CardExample from './Component/CardExample';
+// import Sdata from './Sdata';
 import Mirzapur from './Component/Mirzapur';
 import Cartel from './Component/Cartel';
+import { useState } from 'react';
 
 
 
-const favseries="Cartel"
-const FavS = () => {
-  if(favseries==="Cartel"){
-    return <Cartel />
-  }
-else{
-  <Mirzapur />
-}
-}
+const favseries = "cartel";
+
+// const FavS = () => {
+//   if (favseries === "Cartel") {
+//     return <Cartel />
+//   }
+//   else {
+//     <Mirzapur />
+//   }
+// }
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const IncNum = () => {
+    setCount(count + 1);
+
+  }
+
   // let greeting="kaise ho";
   return (
     <>
@@ -36,7 +46,19 @@ function App() {
         );
       })} */}
 
-      <FavS/>
+      {/* <FavS /> */}
+      {/* {(favseries === "mirzapur")? <Mirzapur />:<Cartel />}    */}
+      <div className="state">
+        <h1>
+          {count}
+        </h1>
+        <button className='button' onClick={IncNum}>CLICK ME</button>
+      </div><br />
+      <div className="state">
+        <h1>time</h1>
+        <button className='button'>GTE TIME</button>
+      </div>
+
     </>
   );
 }
