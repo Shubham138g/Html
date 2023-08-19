9f455461ca880a958f591224f4ecb2649e1db695
shubham <tonygupta275@gmail.com>
2023-08-19 13:31:50 +0530

    making todo list

--- todo-list/todo/src/App.js
+++ todo-list/todo/src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    console.log(event.target.value);
+    setInputList(event.target.value);
+  }
+  const itemOfList = () => {
+
+  }
+
   return (
     <>
-    <div className="main_div">
-      <div className="center_div">
-        <br/>
-        <h1>ToDo List</h1>
-        <br/>
-        <input type="text" placeholder="Add Item" />
-        <button>+</button>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add Item" onChange={itemEvent} />
+          <button onClick={itemOfList}>+</button>
 
-        <ol></ol>
-        <li>buy apple</li>
+          <ol>
+            {/* <li>{inputList} </li> */}
+            {items.map((itemValue) => {
+              return <li>{itemValue} </li>;
+            }
+            )}
+
+          </ol>
+        </div>
+
       </div>
-     
-    </div>
     </>
   );
 }
