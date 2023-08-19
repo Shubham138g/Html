377c90405a020cb182593d0368a6cd3e8838e71f
shubham <tonygupta275@gmail.com>
2023-08-19 14:58:57 +0530

    todo list is done

--- todo-list/todo/src/component/ToDoList.jsx
+++ todo-list/todo/src/component/ToDoList.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
 export default function ToDoList(props) {
-   
   return (
     <>
       <div className="todo_style">
-        <i className="fa fa-times" 
-        aria-hidden="true" 
-        onClick={()=>{
+        <i
+          className="fa fa-times"
+          aria-hidden="true"
+          onClick={() => {
             props.onSelect(props.id);
-        }}></i>
+          }}
+        ></i>
         <li>{props.text} </li>
       </div>
     </>
