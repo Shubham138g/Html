2635456016d9119578d7ed05849360c93ae0462c
shubham <tonygupta275@gmail.com>
2023-08-20 21:02:31 +0530

    adding custom alert box

--- todo-list/todo/src/component/IncDec.jsx
+++ todo-list/todo/src/component/IncDec.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
-import Button from '@material-ui/core/Button';
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+// import Alert from "@material-ui/core/Alert";
+// import Stack from "@material-ui/core/Stack";
+
 
 export default function IncDec() {
   const [num, setNum] = useState(0);
@@ -13,6 +17,11 @@ export default function IncDec() {
       setNum(num - 1);
     } else {
       alert("sorry bro zero limit reached");
+      // <Stack sx={{ width: '100%' }} spacing={2}>
+      // <Alert variant="filled" severity="error">
+      //   This is an error alert — check it out!
+      // </Alert>;
+      // </Stack>
       setNum(0);
     }
   };
@@ -22,13 +31,24 @@ export default function IncDec() {
         <div className="inc_dec_div">
           <h1>{num}</h1>
           <div className="btn">
-            <Button variant="outlined" className="inc_dec_btn inc_btn" onClick={incNum}>
-              <AddIcon />
-            </Button>
-           
-            <Button variant="outlined" className="inc_dec_btn dec_btn" onClick={decNum}>
-              <RemoveIcon />
-            </Button>
+            <Tooltip title="Increment">
+              <Button
+                variant="outlined"
+                className="inc_dec_btn inc_btn"
+                onClick={incNum}
+              >
+                <AddIcon />
+              </Button>
+            </Tooltip>
+            <Tooltip title="Decrement">
+              <Button
+                variant="outlined"
+                className="inc_dec_btn dec_btn"
+                onClick={decNum}
+              >
+                <RemoveIcon />
+              </Button>
+            </Tooltip>
           </div>
         </div>
       </div>
