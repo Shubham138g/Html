23463d75a16eb29976eb3431cbfe6bcd7cd27bf4
shubham <tonygupta275@gmail.com>
2023-08-23 17:11:22 +0530

    working on create note component

--- keepNote/s-note/src/CreateNote.jsx
+++ keepNote/s-note/src/CreateNote.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 
-
 const CreateNote = () => {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const inputEvent = (event) => {
+    const { name, value } = event.target;
+
+    setNote((preValue) => {
+      return {
+        ...preValue,
+        [name]:value,
+      };
+    });
+  };
   return (
     <>
       <div className="container">
         <div className="main_note">
           <form>
-            <input type="text" placeholder="title" autoComplete="off" />
+            <input
+              type="text"
+              name="title"
+              value={note.title}
+              onChange={inputEvent}
+              placeholder="title"
+              autoComplete="off"
+            />
             <textarea
               cols=""
               rows=""
+              name="content"
+              value={note.content}
+              onChange={inputEvent}
               placeholder="Write your note..."
             ></textarea>
-            <Button >
-              <AddIcon className="plus-sign"/>
+            <Button>
+              <AddIcon className="plus-sign" />
             </Button>
           </form>
         </div>
