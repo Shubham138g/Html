36276bfc156d9ead5fd243eafee408f4d4337aed
shubham <tonygupta275@gmail.com>
2023-08-27 21:15:23 +0530

    making a small project

--- miniproject/mini_project/src/Home.jsx
+++ miniproject/mini_project/src/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <div className="col-10 max-auto">
               <div className="col-md-6 pt-5 pt-lg-0 order-2  order-lg-1">
                 <h1>
-                  Grow your business with <strong>MiniProject</strong>
+                  Grow your business with <strong className="brand_name">MiniProject</strong>
                 </h1>
                 <h2 className="my-3">
                   We are the team of talented developers making website
